35224aefcb8a1b52154612d65ad721f8058f0724
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-09-27 20:37:11 +0900

    #3 Change layout

--- app/src/main/java/kr/hs/emirim/sinyh104/imagetest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/imagetest/MainActivity.java
@@ -7,32 +7,49 @@ import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    MyImage mImg;
+    static final int ORIGINAL=0;
+    static final int ROTATE=1;
+    static final int TRANSLATE=2;
+    static final int SCAIL=3;
+    static final int SKEW=4;
+    int choose=ORIGINAL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linear1=(LinearLayout)findViewById(R.id.linear1);
+        mImg=new MyImage(getApplicationContext());
+        linear1.addView(mImg);
+        //linear1.addView(new MyImage(getApplicationContext()));
     }
-//    class MyImage extends View {
-//        MyImage(Context context){
-//            super(context);
-//
-//        }
-//        protected void onDraw(Canvas canvas){
-//            super.onDraw(canvas);
-//
-//            Bitmap picture= BitmapFactory.decodeResource(getResources(),R.drawable.flowers);
-//            float cx=getWidth()/2.0f;
-//            float cy=getHeight()/2.0f;
-//            float x=(getWidth()-picture.getWidth())/2.0f;
-//            float y=(getHeight()-picture.getHeight())/2.0f;
-//            //canvas.rotate(180,cx,cy);//회전
-//            //canvas.translate(-150,200);//이동
-//            //canvas.scale(2,2,cx,cy);//크기변경//2배로
-//            //canvas.skew(0.4f,0.4f);//비틀기(중심축??)
-//            canvas.drawBitmap(picture,x,y,null);
-//        }
-//    }//command + /
+
+    public void transformImage(View v){
+        switch(v.getId()){
+            case R.id.but_rotate:
+                choose=ROTATE;
+                break;
+
+            case R.id.but_translate:
+                choose=TRANSLATE;
+                break;
+
+            case R.id.but_scale:
+                choose=SCAIL;
+                break;
+
+            case R.id.but_skew:
+                choose=SKEW;
+                break;
+        }
+        mImg.setChoose(choose);
+        mImg.invalidate();
+    }
+
+    //command + /
 }
--- app/src/main/java/kr/hs/emirim/sinyh104/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/imagetest/MyImage.java
@@ -10,8 +10,16 @@ import android.view.View;
  * Created by appcreator06 on 2016. 9. 26..
  */
 public class MyImage extends View {
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
+
     MyImage(Context context){
         super(context);
+        picture= BitmapFactory.decodeResource(getResources(),R.drawable.flowers);
+    }
+
+    public void setChoose(int choose) {
+        this.choose = choose;
     }
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
@@ -25,6 +33,24 @@ public class MyImage extends View {
         //canvas.translate(-150,200);//이동
         //canvas.scale(2,2,cx,cy);//크기변경//2배로
         //canvas.skew(0.4f,0.4f);//비틀기(중심축??)
+        //canvas.drawBitmap(picture,x,y,null);
+        switch (choose){
+            case MainActivity.ROTATE:
+                canvas.rotate(180,cx,cy);//회전
+                break;
+            case MainActivity.TRANSLATE:
+                canvas.translate(-150,200);//이동
+                break;
+            case MainActivity.SCAIL:
+                canvas.scale(2,2,cx,cy);//크기변경//2배로
+                break;
+            case MainActivity.SKEW:
+                canvas.skew(0.4f,0.4f);//비틀기(중심축??)
+                break;
+        }
+
         canvas.drawBitmap(picture,x,y,null);
+        picture.recycle();
     }
+
 }
